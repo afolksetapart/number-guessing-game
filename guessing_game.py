37683383534c4aa7e14f5c3e0008af4fe7235fd9
Joe Michelini <josephmichelini@gmail.com>
2020-07-06 13:20:42 -0400

    add initial functions and conditions

--- guessing_game.py
+++ guessing_game.py
@@ -1,44 +1,48 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 import random
 
 
 def start_game():
     print("""
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    Welcome To The Number Guessing Game!
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Welcome To *Spooky* The Number Guessing Game!
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     """)
 
-    solution = random.randint(1, 10)
+    high_score = 0
+# handle errors
+    while True:
+        solution = random.randint(1, 10)
+        tries = 0
 
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+        while True:
+            guess = input("\nPick a number between 1 and 10, if you dare: ")
+            guess = int(guess)
+            tries += 1
+
+            if guess < solution:
+                print("\nAlas, it is higher, my child!")
+            elif guess > solution:
+                print("\nAlas, it is lower, my child!")
+            else:
+                break
+
+        if high_score == 0:
+            high_score = tries
+        elif high_score > tries:
+            high_score = tries
+
+        print("""\nWhat a spooky guess! You are correct!
+        It took you {} bone-chilling tries... 
+        """.format(tries))
+
+        play_again = input("Do you dare risk another round? [y / n]: ")
+
+        if play_again.lower() == "y":
+            print("\nThe current *~high score~* is {}".format(high_score))
+            continue
+        break
+
+    print("\nToo scared to guess again?! Have it your way! Bwa-ha-ha-ha!\n")
 
 
-# Kick off the program by calling the start_game function.
 start_game()
